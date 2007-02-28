9ca518ceea4b051e52b04aca91e71e95ad940bbf
Kern Sibbald <kern@sibbald.com>
2007-02-28 10:33:19 +0000

    28Feb07 kes  Correct typeo in var.c patch. 27Feb07 kes  Don't let Bacula prune File or Job records for the current Job. kes  Fix variable substitution pad + inc bug reported (with patch)      in bug #791. 26Feb07 kes  Correct SQLite log table index as reported by Luca Berra
    git-svn-id: https://bacula.svn.sourceforge.net/svnroot/bacula/branches/Branch-2.0@4278 91ce42f0-d328-0410-95d8-f526ca767f89

--- bacula/src/version.h
+++ bacula/src/version.h
@@ -4,8 +4,8 @@
 
 #undef  VERSION
 #define VERSION "2.0.3"
-#define BDATE   "22 February 2007"
-#define LSMDATE "22Feb07"
+#define BDATE   "28 February 2007"
+#define LSMDATE "28Feb07"
 
 #define PROG_COPYRIGHT "Copyright (C) %d-2007 Free Software Foundation Europe e.V.\n"
 #define BYEAR "2007"       /* year for copyright messages in progs */
