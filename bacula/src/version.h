78a3baabbf2008d860f1d030a3e55942f657d98e
Kern Sibbald <kern@sibbald.com>
2007-11-10 09:00:23 +0000

    Update doc for release
    git-svn-id: https://bacula.svn.sourceforge.net/svnroot/bacula/branches/Branch-2.2@5882 91ce42f0-d328-0410-95d8-f526ca767f89

--- bacula/src/version.h
+++ bacula/src/version.h
@@ -4,8 +4,8 @@
 
 #undef  VERSION
 #define VERSION "2.2.6"
-#define BDATE   "09 November 2007"
-#define LSMDATE "09Nov07"
+#define BDATE   "10 November 2007"
+#define LSMDATE "10Nov07"
 
 #define PROG_COPYRIGHT "Copyright (C) %d-2007 Free Software Foundation Europe e.V.\n"
 #define BYEAR "2007"       /* year for copyright messages in progs */
