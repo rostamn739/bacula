ff96a9f3e5217a79a432c6492ef0f6097f63fc23
Kern Sibbald <kern@sibbald.com>
2008-04-14 08:24:39 +0000

    Add missing variable definitions to ua_prune.c
    git-svn-id: https://bacula.svn.sourceforge.net/svnroot/bacula/branches/Branch-2.2@6813 91ce42f0-d328-0410-95d8-f526ca767f89

--- bacula/src/dird/ua_prune.c
+++ bacula/src/dird/ua_prune.c
@@ -428,6 +428,8 @@ int get_prune_list_for_volume(UAContext *ua, MEDIA_DBR *mr, del_ctx *del)
    int i;          
    utime_t now, period;
    char ed1[50], ed2[50];
+   JCR *jcr;
+   bool skip;
 
    if (mr->Enabled == 2) {
       return 0;                    /* cannot prune Archived volumes */
