9c533be269aad648700bcaabc9b7b3722992af30
Kern Sibbald <kern@sibbald.com>
2008-05-11 14:38:19 +0000

    Apply batfix bat cleanup patch from Riccardo.
    git-svn-id: https://bacula.svn.sourceforge.net/svnroot/bacula/branches/Branch-2.2@6948 91ce42f0-d328-0410-95d8-f526ca767f89

--- bacula/src/qt-console/fileset/fileset.cpp
+++ bacula/src/qt-console/fileset/fileset.cpp
@@ -132,7 +132,7 @@ void FileSet::populateTree()
       }
    }
    /* Resize the columns */
-   for (int cnter=1; cnter<headerlist.size(); cnter++) {
+   for (int cnter=0; cnter<headerlist.size(); cnter++) {
       mp_treeWidget->resizeColumnToContents(cnter);
    }
 
