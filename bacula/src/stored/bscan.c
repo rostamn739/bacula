832733084dbd6b44ce608dbd5f1a8b6a7da1a6c8
Kern Sibbald <kern@sibbald.com>
2008-06-25 22:14:10 +0000

    Fix format problem in bscan output reported in bug #1105.
    git-svn-id: https://bacula.svn.sourceforge.net/svnroot/bacula/branches/Branch-2.4@7237 91ce42f0-d328-0410-95d8-f526ca767f89

--- bacula/src/stored/bscan.c
+++ bacula/src/stored/bscan.c
@@ -277,7 +277,7 @@ int main (int argc, char *argv[])
       struct stat sb;
       fstat(dev->fd(), &sb);
       currentVolumeSize = sb.st_size;
-      Pmsg1(000, _("First Volume Size = %sn"), 
+      Pmsg1(000, _("First Volume Size = %s\n"), 
          edit_uint64(currentVolumeSize, ed1));
    }
 
@@ -352,7 +352,7 @@ static bool bscan_mount_next_read_volume(DCR *dcr)
       struct stat sb;
       fstat(dev->fd(), &sb);
       currentVolumeSize = sb.st_size;
-      Pmsg1(000, _("First Volume Size = %sn"), 
+      Pmsg1(000, _("First Volume Size = %s\n"), 
          edit_uint64(currentVolumeSize, ed1));
    }
    return stat;
