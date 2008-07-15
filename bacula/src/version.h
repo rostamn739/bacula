7577304281801bd4ca6429aef3329e15de84f8be
Kern Sibbald <kern@sibbald.com>
2008-07-15 18:16:36 +0000

    kes  Ensure that SD tried to mount a volume not in an autochanger      at least once before asking for operator intervention.
    git-svn-id: https://bacula.svn.sourceforge.net/svnroot/bacula/branches/Branch-2.4@7389 91ce42f0-d328-0410-95d8-f526ca767f89

--- bacula/src/version.h
+++ bacula/src/version.h
@@ -4,8 +4,8 @@
 
 #undef  VERSION
 #define VERSION "2.4.1"
-#define BDATE   "07 July 2008"
-#define LSMDATE "07Jul08"
+#define BDATE   "15 July 2008"
+#define LSMDATE "15Jul08"
 
 #define PROG_COPYRIGHT "Copyright (C) %d-2008 Free Software Foundation Europe e.V.\n"
 #define BYEAR "2008"       /* year for copyright messages in progs */
