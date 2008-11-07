7f487b6ad1544034f80e46c01cec11b352186b2d
Kern Sibbald <kern@sibbald.com>
2008-11-07 16:26:48 +0000

    kes  Fix bug with job name duplication if more than 60 jobs created      during a minute. kes  Correct some bugs of cleanup in SD if the FD connection fails.
    git-svn-id: https://bacula.svn.sourceforge.net/svnroot/bacula/branches/Branch-2.4@8012 91ce42f0-d328-0410-95d8-f526ca767f89

--- bacula/src/version.h
+++ bacula/src/version.h
@@ -3,9 +3,9 @@
  */
 
 #undef  VERSION
-#define VERSION "2.4.3"
-#define BDATE   "28 October 2008"
-#define LSMDATE "28Oct08"
+#define VERSION "2.4.4"
+#define BDATE   "07 November 2008"
+#define LSMDATE "07Nov08"
 
 #define PROG_COPYRIGHT "Copyright (C) %d-2008 Free Software Foundation Europe e.V.\n"
 #define BYEAR "2008"       /* year for copyright messages in progs */
