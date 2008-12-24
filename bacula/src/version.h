fe947c9cb99bf109b99fa4a65aa3bf162f5eefa2
Kern Sibbald <kern@sibbald.com>
2008-12-24 09:06:57 +0000

    Final doc updates for 2.4.4-b2
    git-svn-id: https://bacula.svn.sourceforge.net/svnroot/bacula/branches/Branch-2.4@8246 91ce42f0-d328-0410-95d8-f526ca767f89

--- bacula/src/version.h
+++ bacula/src/version.h
@@ -4,8 +4,8 @@
 
 #undef  VERSION
 #define VERSION "2.4.4-b2"
-#define BDATE   "22 December 2008"
-#define LSMDATE "22Dec08"
+#define BDATE   "24 December 2008"
+#define LSMDATE "24Dec08"
 
 #define PROG_COPYRIGHT "Copyright (C) %d-2008 Free Software Foundation Europe e.V.\n"
 #define BYEAR "2008"       /* year for copyright messages in progs */
