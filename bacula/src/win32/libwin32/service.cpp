1ee90f11ae3a74973d8c113d2b7b3e230874138a
Kern Sibbald <kern@sibbald.com>
2011-01-02 11:13:51 +0100

    Remove tray-monitor from Win32 build

--- bacula/src/win32/libwin32/service.cpp
+++ bacula/src/win32/libwin32/service.cpp
@@ -110,7 +110,7 @@ void WINAPI serviceStartCallback(DWORD argc, char **argv)
       return;
    }
 
-   service_status.dwServiceType = SERVICE_WIN32|SERVICE_INTERACTIVE_PROCESS;
+   service_status.dwServiceType = SERVICE_WIN32;
    service_status.dwServiceSpecificExitCode = 0;
 
    /* Report status */
